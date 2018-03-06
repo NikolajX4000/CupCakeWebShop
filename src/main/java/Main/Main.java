7b310388606638969786c53cf6b0fe5dde034501
Hupra Laptop <Hupra Laptop@10.50.137.243>
2018-03-06 09:33:49 +0100

    test.html

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -5,7 +5,6 @@ import DBConnection.DAO;
 public class Main {
 
     public static void main(String[] args) {
-        DAO dao = new DAO();
-        //System.out.println(dao.withdrawal(1, 46));
+        System.out.println("Daniel skriver her :)");
     }
 }
