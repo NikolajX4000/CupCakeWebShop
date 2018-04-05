cc194c0f7a1aab7a123f56016e72750e0c271df7
Stephan.dd <Stephan@192.168.0.17>
2018-04-05 23:31:42 +0200

    JUnit test @100%

--- src/test/java/DBConnection/DAOTest.java
+++ src/test/java/DBConnection/DAOTest.java
@@ -23,25 +23,6 @@ import static org.junit.Assert.*;
  */
 public class DAOTest {
     
-    public DAOTest() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
     /**
      * Test of getUser method, of class DAO.
      */
